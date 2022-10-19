fee24e93d01228f03b2ca3da85ab9d80bafbf63a
Evgenii Kolgin <mista.colgate@gmail.com>
2022-10-19 18:20:55 +0800

    testing scheduled message

--- Sources/App/DefaultBotHandlers.swift
+++ Sources/App/DefaultBotHandlers.swift
@@ -1,14 +1,5 @@
 import telegram_vapor_bot
 import Vapor
-import Queues
-
-struct CleanupJob: AsyncScheduledJob {
-    // Add extra services here via dependency injection, if you need them.
-
-    func run(context: QueueContext) async throws {
-        // Do some work here, perhaps queue up another job.
-    }
-}
 
 final class DefaultBotHandlers {
     
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,19 @@
 import telegram_vapor_bot
 import Vapor
+import Queues
+
+struct CleanupJob: AsyncScheduledJob {
+    // Add extra services here via dependency injection, if you need them.
+
+    let bot: TGBot
+    
+    func run(context: QueueContext) async throws {
+        // Do some work here, perhaps queue up another job.
+        let chatId: TGChatId = .chat(-1001804864589)
+        let params:TGSendMessageParams = .init(chatId: chatId, text: "This is scheduled message")
+        try bot.sendMessage(params: params)
+    }
+}
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -15,4 +29,6 @@ public func configure(_ app: Application) throws {
     TGBot.log.logLevel = .error
     
     DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
+    
+    app.queues.schedule(CleanupJob(bot: TGBot.shared)).daily().at(18, 30)
 }
