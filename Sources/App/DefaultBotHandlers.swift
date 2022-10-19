ff50482bc96a70fb8fd06bb5e47ff4bd223bebf6
Evgenii Kolgin <mista.colgate@gmail.com>
2022-10-19 16:53:45 +0800

    get resource method

--- Sources/App/DefaultBotHandlers.swift
+++ Sources/App/DefaultBotHandlers.swift
@@ -23,27 +23,45 @@ final class DefaultBotHandlers {
         let handler = TGMessageHandler(filters: .command.names(["/start"])) { update, bot in
             guard let message = update.message else { return }
             let chatId: TGChatId = .chat(message.chat.id)
-
-            let headers: HTTPHeaders = HTTPHeaders([("Authorization", "Bearer \(Environment.get("BEARER_TOKEN")!)")])
             let url = TwitterApi.getIdByUsername("elonmusk").url
-            print(url)
-            let uri: URI = URI(string: "https://api.twitter.com/2/users/by/username/elonmusk")
             
-            app.client.get(uri, headers: headers).flatMapThrowing { response in
-                guard response.status == .ok else { throw Abort(.unauthorized) }
-                guard let buffer = response.body else { throw Abort(.badRequest) }
-                guard let data = String(buffer: buffer).data(using: .utf8) else { throw Abort(.badRequest) }
-                
-                do {
-                    let data = try JSONDecoder().decode(Response.self, from: data)
-                    let params:TGSendMessageParams = .init(chatId: chatId, text: "\(data.data.name): \(url)")
-                    try bot.sendMessage(params: params)
-                } catch {
-                    let params:TGSendMessageParams = .init(chatId: chatId, text: "Error")
+            do {
+                let response = try getResourceOf(type: Response.self, for: url, app: app).flatMapThrowing { result in
+                    let params:TGSendMessageParams = .init(chatId: chatId, text: "\(result.data.name)")
                     try bot.sendMessage(params: params)
                 }
+            } catch {
+                throw Abort(.custom(code: 3, reasonPhrase: "Failed to get resource"))
             }
         }
         bot.connection.dispatcher.add(handler)
+    }
+}
+
+extension DefaultBotHandlers {
+    private static func getResourceOf<T:Codable>(type: T.Type, for url: URL?, app: Vapor.Application) throws -> EventLoopFuture<T> {
+        
+        guard let urlString = url?.absoluteString else {
+            throw Abort(.custom(code: 1, reasonPhrase: "Couldn't convert url to string"))
+        }
+        
+        guard let token = Environment.get("BEARER_TOKEN") else {
+            throw Abort(.custom(code: 1, reasonPhrase: "No Bearer Token Variable"))
+        }
+
+        let headers: HTTPHeaders = HTTPHeaders([("Authorization", "Bearer \(token)")])
+        
+        return app.client.get(URI(string: urlString), headers: headers).flatMapThrowing { response in
+            guard response.status == .ok else { throw Abort(.unauthorized) }
+            guard let buffer = response.body else { throw Abort(.badRequest) }
+            guard let data = String(buffer: buffer).data(using: .utf8) else { throw Abort(.badRequest) }
+            
+            do {
+                let data = try JSONDecoder().decode(type.self, from: data)
+                return data
+            } catch {
+                throw Abort(.custom(code: 3, reasonPhrase: "Failed decoding JSON"))
+            }
+        }
     }
 }
